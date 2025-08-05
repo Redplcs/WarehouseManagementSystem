c6f05bfc915368e35e1be1868a05fa210de1b6aa
Redplcs <51379496+Redplcs@users.noreply.github.com>
2025-08-05 20:24:32 +0500

    Add dependency injection on each layer

--- src/Warehouse.Api/Program.cs
+++ src/Warehouse.Api/Program.cs
@@ -1,7 +1,15 @@
+using Warehouse.Api;
+using Warehouse.Application;
+using Warehouse.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+builder.Services.AddPresentation();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
