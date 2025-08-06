dda5f5a7dffd06b977ef6af240f7a6e33f973c37
Redplcs <51379496+Redplcs@users.noreply.github.com>
2025-08-06 20:40:18 +0500

    Add entity abstraction

---
+++ src/Warehouse.Shared/Entity.cs
@@ -0,0 +1,31 @@
+﻿namespace Warehouse.Shared;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public Guid Id { get; init; }
+
+    public bool Equals(Entity? other)
+    {
+        return other is not null && Id == other.Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Entity entity && Equals(entity);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(Entity? left, Entity? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Entity? left, Entity? right)
+    {
+        return !Equals(left, right);
+    }
+}
